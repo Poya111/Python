07185e8dbf639a82c08ca6a3bfbc20ba8fc04d70
Poya <hamid24150@yahoo.com>
2020-08-01 08:42:15 -0700

    Modification mthod for converting from any bases to any bases

--- RecursiveBase10.py
+++ RecursiveBase10.py
@@ -1,21 +1,30 @@
 result = []
+flage = True
 def from_base10(n, b):
     """This function is used for converting bases.
     param n: integer
     param b: integer
     return:  list
     """
+    global flage
+
     if b < 2:
         raise ValueError ('Base must greater than 2.')
 
-    m , n = n % b, n // b
-    result.insert(0,m)
+    if n < b and flage  == True:
+        return result.insert(0,n)
+    else:
+        flage = False
+        m , n = n % b, n // b
+        result.insert(0,m)
+        if n == 0:
+            return [0]
+        return from_base10(n, b)
 
-    if n == 0:
-        return [0]
-    return from_base10(n, b)
 
 
-from_base10(6,2)
+
+
+from_base10(4,2)
 print(result)
-help(from_base10)
+#help(from_base10)
