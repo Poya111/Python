f4a7b565ae8265051578e4f4b447cd6af9143bb8
Poya <hamid24150@yahoo.com>
2020-07-31 19:09:36 -0700

    Add comment for using help function

--- RecursiveBase10.py
+++ RecursiveBase10.py
@@ -1,16 +1,21 @@
 result = []
 def from_base10(n, b):
+    """This function is used for converting bases.
+    param n: integer
+    param b: integer
+    return:  list
+    """
     if b < 2:
         raise ValueError ('Base must greater than 2.')
 
-    #m = n % b
-    #n = n // b
     m , n = n % b, n // b
     result.insert(0,m)
-    print(n)
+
     if n == 0:
         return [0]
     return from_base10(n, b)
 
-from_base10(4,2)
+
+from_base10(6,2)
 print(result)
+help(from_base10)
